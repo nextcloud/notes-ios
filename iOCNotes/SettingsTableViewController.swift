f98e38b0977c06665b480c36ed93f93e2f6a36ba
Marino Faggiana <96728420+MarinoFaggianaAstraIridium@users.noreply.github.com>
2023-02-13 17:08:21 +0100

    clear code
    Signed-off-by: Marino Faggiana <96728420+MarinoFaggianaAstraIridium@users.noreply.github.com>

--- iOCNotes/SettingsTableViewController.swift
+++ iOCNotes/SettingsTableViewController.swift
@@ -76,39 +76,6 @@ class SettingsTableViewController: UITableViewController {
             if indexPath.row == 1 {
                 showNotesFolderAlert()
             }
-        case 3:
-            if indexPath.row == 0 {
-                let email = "support@pbh.dev"
-                let subject = NSLocalizedString("NextcloudNotes Support Request", comment: "Support email subject")
-                let body = NSLocalizedString("<Please state your question or problem here>", comment: "Support email body placeholder")
-                if MFMailComposeViewController.canSendMail() {
-                    let mailViewController = MFMailComposeViewController()
-                    mailViewController.mailComposeDelegate = self
-                    mailViewController.setToRecipients([email])
-                    mailViewController.setSubject(subject)
-                    mailViewController.setMessageBody(body, isHTML: false)
-                    mailViewController.modalPresentationStyle = .formSheet;
-                    present(mailViewController, animated: true, completion: nil)
-                } else {
-                    var components = URLComponents()
-                    components.scheme = "mailto"
-                    components.path = email
-                    components.queryItems = [URLQueryItem(name: "subject", value: subject),
-                                             URLQueryItem(name: "body", value: body)]
-                    if let mailURL = components.url {
-                        if UIApplication.shared.canOpenURL(mailURL) {
-                            UIApplication.shared.open(mailURL, options: [:], completionHandler: nil)
-                        } else {
-                            // No email client configured
-                        }
-                    }
-                }
-            } else if indexPath.row == 1 {
-                if let url = URL(string: "https://pbh.dev/cloudnotes") {
-                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                }
-            }
-
         default:
             break
         }
