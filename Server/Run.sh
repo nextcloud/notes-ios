efe68d03df36a7bd5ed138036ebe510f5955ea4f
Iva Horn <iva.horn@icloud.com>
2025-03-05 14:22:24 +0100

    Fixed provisioned test server.
    For details, see Server/README.md
    
    Signed-off-by: Iva Horn <iva.horn@icloud.com>

--- Server/Run.sh
+++ Server/Run.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env zsh
 
-SCRIPT_DIR=$(dirname "$0")
-CONTAINER_NAME="nextcloud-notes-server"
+CONTAINER_NAME="nextcloud-notes-test-server"
 NEXTCLOUD_TRUSTED_DOMAINS="${NEXTCLOUD_TRUSTED_DOMAINS:-localhost}"
+NEXTCLOUD_URL="http://localhost:8080"
+POLL_INTERVAL=1
+SCRIPT_DIR=$(dirname "$0")
 
 docker run \
     --name $CONTAINER_NAME \
@@ -14,8 +16,28 @@ docker run \
     --env NEXTCLOUD_TRUSTED_DOMAINS="$NEXTCLOUD_TRUSTED_DOMAINS" \
     nextcloud
 
+echo "Waiting for Nextcloud to be set up..."
+
+while true; do
+  if ! docker ps --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
+    echo "Container '$CONTAINER_NAME' is not running. Waiting $POLL_INTERVAL seconds..."
+    sleep "$POLL_INTERVAL"
+    continue
+  fi
+
+  response=$(curl -s "$NEXTCLOUD_URL/status.php")
+
+  if echo "$response" | grep -q '"installed":[[:space:]]*true'; then
+    echo "Nextcloud is set up!"
+    break
+  else
+    echo "Nextcloud is not set up yet. Waiting $POLL_INTERVAL seconds..."
+    sleep "$POLL_INTERVAL"
+  fi
+done
+
+echo "Provisioning..."
 docker cp "$SCRIPT_DIR/Provisioning.sh" "$CONTAINER_NAME":/var/www/html/Provisioning.sh
-echo "Waiting 10 seconds for the container to be ready before executing provisioning script..."
-sleep 5
 docker exec --user=www-data "$CONTAINER_NAME" ./Provisioning.sh
 docker cp "$SCRIPT_DIR/Notes" "$CONTAINER_NAME":/var/www/html/data/manynotes/files/
+echo "Provisioning completed!"
