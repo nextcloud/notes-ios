d6d4671e0a5263f379cc180d548222a6d3c6f610
Iva Horn <iva.horn@icloud.com>
2025-04-25 15:05:42 +0200

    Renamed "NotesScreen" to "NotesView" and moved it into the buildable folder for the associated target.
    Signed-off-by: Iva Horn <iva.horn@icloud.com>

--- iOCNotes/Views/ContentView.swift
+++ iOCNotes/Views/ContentView.swift
@@ -8,7 +8,7 @@ struct ContentView: View {
     var body: some View {
         TabView {
             NavigationStack {
-                NotesScreen()
+                NotesView()
             }
             .tabItem {
                 Label(
