51706f570ada388c635e75ad82970aa1870cef3c
Iva Horn <iva.horn@icloud.com>
2025-06-13 16:49:14 +0200

    Moved UIApplication+topViewController extension into dedicated source code file for improved organization.
    Signed-off-by: Iva Horn <iva.horn@icloud.com>

---
+++ Source/UIApplication+topViewController.swift
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: Nextcloud GmbH
+// SPDX-FileCopyrightText: 2025 Iva Horn
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import UIKit
+
+extension UIApplication {
+    class func topViewController(base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(base: nav.visibleViewController)
+        }
+        
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(base: selected)
+            }
+        }
+
+        if let presented = base?.presentedViewController {
+            return topViewController(base: presented)
+        }
+
+        return base
+    }
+}
