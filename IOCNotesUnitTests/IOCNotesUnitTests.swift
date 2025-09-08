7e5c934886752e622841876f1018ae8fea17f8f7
Milen Pivchev <milen.pivchev@gmail.com>
2025-09-08 11:01:33 +0200

    WIP
    Signed-off-by: Milen Pivchev <milen.pivchev@gmail.com>

---
+++ IOCNotesUnitTests/IOCNotesUnitTests.swift
@@ -0,0 +1,17 @@
+//
+//  IOCNotesUnitTests.swift
+//  IOCNotesUnitTests
+//
+//  Created by Milen Pivchev on 08.09.25.
+//  Copyright © 2025 Nextcloud GmbH. All rights reserved.
+//
+
+import Testing
+
+struct IOCNotesUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
--- iOCNotesTests/MarkdownTextStorageTests.swift
+++ iOCNotesTests/MarkdownTextStorageTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import iOCNotes
 
 class MarkdownTextStorageTests: XCTestCase {
     
